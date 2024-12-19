d8f1375020a204c285a2b8246f76e9d0b72e90fd
ttiffiX <sangpham1224@gmail.com>
2024-12-19 14:02:09 +0700

    first commit

--- sale-tech-web/src/main/java/com/example/sale_tech_web/feature/product/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.sale_tech_web.feature.product.repository;
-
-import com.example.sale_tech_web.feature.product.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
